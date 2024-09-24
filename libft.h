471ab1028148d90270b8f5f2198204d617c1a899
Lando Rafael <lrafael@c1r7s10.42luanda.com>
2024-09-24 08:35:21 +0100

    new functions added

--- libft.h
+++ libft.h
@@ -14,6 +14,8 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# include "./get_next_line/get_next_line.h"
+# include "./ft_printf/ft_printf.h"
 
 int			ft_isalpha(int c);
 int			ft_isdigit(int c);
